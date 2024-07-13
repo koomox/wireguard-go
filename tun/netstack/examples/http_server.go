eea20725c80655f8866c0c7f5820b804744f16a8
allen.w <koomoxs@gmail.com>
2024-07-13 21:32:56 +0800

    commit

--- tun/netstack/examples/http_server.go
+++ tun/netstack/examples/http_server.go
@@ -14,9 +14,9 @@ import (
 	"net/http"
 	"net/netip"
 
-	"golang.zx2c4.com/wireguard/conn"
-	"golang.zx2c4.com/wireguard/device"
-	"golang.zx2c4.com/wireguard/tun/netstack"
+	"github.com/koomox/wireguard-go/conn"
+	"github.com/koomox/wireguard-go/device"
+	"github.com/koomox/wireguard-go/tun/netstack"
 )
 
 func main() {
